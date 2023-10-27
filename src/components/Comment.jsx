23edc982d67b5e1364705f779523395b8ed5c580
joao vitor <jvitorgit@gmail.com>
2023-10-26 22:47:34 -0300

    Creating Avatar Component and doing refactoring

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,17 @@
 import { Trash, ThumbsUp } from 'phosphor-react'
 
 import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
 
 export function Comment() {
     return (
         <div className={styles.comment}>
-            <img src="https://github.com/joaoVitorDS12.png" />
+            <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
-                            <strong>João Vitor</strong>
+                            <strong>Diego Fernandes</strong>
                             <time title="25 de Outubro as 22:25" dateTime="2023-10-25 22:25:30">Cerca de 1h atrás</time>
                         </div>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,10 +7,10 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/joaoVitorDS12.png" />
+                    <Avatar src="https://github.com/maykbrito.png" />
                     <div className={styles.authorInfo}>
-                        <strong>João Vitor</strong>
-                        <span>Web Developer</span>
+                        <strong>Mayk Brito</strong>
+                        <span>Educator</span>
                     </div>
                 </div>
                 
